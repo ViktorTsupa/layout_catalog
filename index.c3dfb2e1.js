6a2fc7cf36f18596f1c1ccdfda30df2267ec5f20
Viktor <victor.tsupa@gmail.com>
2024-02-07 19:04:02 +0200

    make build

---
+++ index.c3dfb2e1.js
@@ -0,0 +1,3 @@
+const e=document.querySelector(".body"),t=document.querySelector(".hamburger"),c=document.querySelector(".header__list");t.addEventListener("click",()=>{t.classList.toggle("active"),c.classList.toggle("active"),e.classList.toggle("active")});//# sourceMappingURL=index.c3dfb2e1.js.map
+
+//# sourceMappingURL=index.c3dfb2e1.js.map
